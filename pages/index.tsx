cba304b0d1a7441fa1fb84bd6fd387af27f2952d
Aryan S <aryan.swamianth@aol.com>
2023-04-15 21:17:28 -0500

    feat: update root index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,8 @@
 import Link from 'next/link';
 import * as React from 'react';
-import { TbBrandGithub, TbBrandVercel, TbHelp } from 'react-icons/tb';
 
 import { Breadcrumbs } from '@/components/layout/Breadcrumbs';
 import Layout from '@/components/layout/Layout';
-import { BigLogo } from '@/components/Logo';
 import Seo from '@/components/Seo';
 
 export default function HomePage() {
@@ -20,30 +18,24 @@ export default function HomePage() {
             <div className='hero'>
               <div className='hero-content text-center'>
                 <div className='flex max-w-md flex-col items-center justify-center'>
-                  <BigLogo />
-                  <h1 className='h1 mt-4'>Web Starter</h1>
+                  <h1 className='h1 mt-4'>Numerical Analysis</h1>
                   <h3 className='h3 mt-4 flex max-w-md flex-wrap gap-2'>
-                    Next.js, TypeScript, DaisyUI, TailwindCSS, Supabase, MDX,
-                    React Hot Toast, and more
+                    Explore ways to solve equations and find roots using various
+                    numerical methods, including the Bisection Method,
+                    Fixed-Point Iteration, and more.
                   </h3>
                   <div className='btn-group btn-group-vertical mt-6'>
-                    <Link className='btn' href='/features'>
-                      <TbHelp className='mr-2' />
-                      All features
+                    {/* Add methods page */}
+                    <Link className='btn' href='/'>
+                      Explore Methods
                     </Link>
-                    <Link
-                      className='btn'
-                      href='https://github.com/jeffreyhugh/starter'
-                    >
-                      <TbBrandGithub className='mr-2' />
-                      See it on GitHub
+                    {/* Add compare page */}
+                    <Link className='btn' href='/'>
+                      Compare Methods
                     </Link>
-                    <Link
-                      className='btn'
-                      href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Fjeffreyhugh%2Fstarter'
-                    >
-                      <TbBrandVercel className='mr-2' />
-                      Deploy on Vercel
+                    {/* Remove in prod */}
+                    <Link className='btn' href='/playground'>
+                      Playground
                     </Link>
                   </div>
                 </div>
